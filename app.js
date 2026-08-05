b699875c2f733e13080ae2ae366072585891bf9f
sanket264 <ghadagesanket48@gmail.com>
2026-08-05 12:30:46 +0530

    Fix Render port configuration

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ app.use((err,req,res,next)=>{
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080,()=>{
-    console.log("Server is running on port 8080");
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8080}`);
 });
